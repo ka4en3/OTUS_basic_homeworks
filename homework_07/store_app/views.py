4a3e99562e3e735a574fe91b849867965b190761
ka4en3 <ka4en3@gmail.com>
2025-03-19 19:53:53 +0100

    19mar2025

--- homework_07/store_app/views.py
+++ homework_07/store_app/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse(f"Main page. Request: {request}")
+    return HttpResponse(
+        f"<p>Main page</p>Request: {request.user}<br>GET:<br>{request.GET}<br>META:<br>{request.META}"
+    )
 
 
 def about(request):
